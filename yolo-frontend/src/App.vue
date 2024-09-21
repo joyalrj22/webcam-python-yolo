fab3f90a4b5c7fe837211f82fcf72b6e9879f0ce
joyalOXMT <joyal.raju@oxmt.net>
2024-09-21 14:07:53 +0100

    video stream component

--- yolo-frontend/src/App.vue
+++ yolo-frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VideoStream />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/VideoStream.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VideoStream
   }
 }
 </script>
---
+++ yolo-frontend/src/components/VideoStream.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Live Streaming Video</h1>
+    <img :src="videoFeedUrl" alt="Live Stream" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videoFeedUrl: 'http://localhost:5000/video_feed'
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+img {
+  width: 80%;
+  height: auto;
+  border: 2px solid #000;
+}
+</style>
